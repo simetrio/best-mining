fb5a3ea857aa3ff8651a718d047c8ffbd1d4a2a4
simetrio <simetrio@yandex.com>
2024-05-09 10:32:50 +0500

    Делаю публикацию страниц

---
+++ bestmining-tg-channel/BestMiningTgChannel/BestMiningTgChannel.Tests/PublisherTests.cs
@@ -0,0 +1,62 @@
+using System.Diagnostics;
+using BestMiningTgChannel.Publisher;
+
+namespace BestMiningTgChannel.Tests;
+
+public class PublisherTests
+{
+    [Fact]
+    public void Publish()
+    {
+        var sourceRootDirectory = "/home/roman/projects/best-mining/best-mining/pages";
+        var destinationRootDirectory = "/home/roman/projects/best-mining/best-mining/docs";
+        var master = File.ReadAllText(Path.Combine(sourceRootDirectory, "master.html"));
+
+        PublishInner(sourceRootDirectory, "", destinationRootDirectory, master);
+    }
+
+    private void PublishInner(
+        string sourceRootDirectory,
+        string subDirectory,
+        string destinationRootDirectory,
+        string master
+    )
+    {
+        var sourceDirectory = Path.Combine(sourceRootDirectory, subDirectory);
+        var destinationDirectory = Path.Combine(destinationRootDirectory, subDirectory);
+
+        if (!Directory.Exists(destinationDirectory))
+        {
+            Directory.CreateDirectory(destinationDirectory);
+        }
+
+        var files = Directory
+            .GetFiles(sourceDirectory)
+            .Select(x => new FileInfo(x).Name)
+            .Where(x => x != "master.html");
+
+        var directories = Directory
+            .GetDirectories(sourceDirectory)
+            .Select(x => new DirectoryInfo(x).Name);
+
+        foreach (var file in files)
+        {
+            var lines = File.ReadAllLines(Path.Combine(sourceDirectory, file));
+            var title = lines[0];
+            var content = string.Join(Environment.NewLine, lines.Skip(1));
+
+            var result = master.Replace("{title}", title).Replace("{content}", content);
+            File.WriteAllText(Path.Combine(destinationDirectory, file), result);
+        }
+
+        foreach (var directory in directories)
+        {
+            PublishInner(
+                sourceRootDirectory,
+                Path.Combine(subDirectory, directory),
+                destinationRootDirectory,
+                master
+            );
+        }
+    }
+}
