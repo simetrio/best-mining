4d666fc1717eb780fbc938ba1e6a84cbcffdaedf
simetrio <simetrio@yandex.com>
2024-05-09 12:07:17 +0500

    Загружаю товары

--- docs/js/site.js
+++ docs/js/site.js
@@ -16,3 +16,30 @@ function openTgModal() {
 function closeTgModal() {
     localStorage.setItem(getTgModalName(), 'true');
 }
+
+async function loadData() {
+    const prices = await (await fetch("/data/prices.json")).json();
+    const products = await (await fetch("/data/products.json")).json();
+
+    prices.sort(x => x.Value);
+
+    products.forEach(product => {
+        product.Price = toRuble(prices.filter(x => x.Id === product.Id)[0].Value);
+    });
+
+    Storage.Products = products;
+    console.log(products);
+}
+
+function toRuble(price) {
+    const dollar = 98;
+    return price * dollar;
+}
+
+const Storage = {}
+
+function main() {
+    openTgModal();
+    loadData();
+    showCatalog();
+}
